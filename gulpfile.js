a23322d88e499805c5dc41843cd3d8e3bda1ae94
Harrison <hwstovall@gmail.com>
2015-08-08 18:39:22 -0700

    Built react.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var react = require('gulp-react');
 
 gulp.task('default', function(){
     return gulp.src('./src/EasyDay.js')
         .pipe(babel())
+        .pipe(react())
         .pipe(gulp.dest('./lib'))
 });
